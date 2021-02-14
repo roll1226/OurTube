0d3bf0d13c8f12c5fa8490b5720263f50e7e25f8
roll1226 <tomo_yasu_4724@icloud.com>
2021-02-14 19:28:35 +0900

    delete: sliceの削除

--- src/components/molecules/GoogleSignInButtonMolecules.tsx
+++ src/components/molecules/GoogleSignInButtonMolecules.tsx
@@ -2,14 +2,9 @@ import ButtonAtoms from "../atoms/ButtonAtoms"
 import GeneralColorStyle from "../../styles/colors/GeneralColorStyle"
 import GoogleIconAtoms from "../atoms/svg/GoogleIconAtoms"
 import FirebaseAuthenticationUtil from "../../utils/lib/FirebaseAuthenticationUtil"
-import { useDispatch } from "react-redux"
-import authSlice from "../../ducks/auth/slice"
 
 const GoogleSignInButtonMolecules = () => {
-  const dispatch = useDispatch()
-
   const signInGoogle = async () => {
-    dispatch(authSlice.actions.setAuthLoader(true))
     await FirebaseAuthenticationUtil.signInForGoogle()
   }
 
