c1e26df458879cea9c43521631b66b7151893ed1
roll1226 <tomo_yasu_4724@icloud.com>
2021-02-27 01:57:05 +0900

    feat: ヘッダーログにリンク

--- src/components/molecules/AccountHeadMolecules.tsx
+++ src/components/molecules/AccountHeadMolecules.tsx
@@ -1,19 +1,23 @@
 import styled from "styled-components"
 import OurTubeLogoAtoms, { LogoColor } from "../atoms/svg/OurTubeLogoAtoms"
 import SignOutButtonMolecules from "./SignOutButtonMolecules"
+import Link from "next/link"
 
 const LogoContainer = styled.div`
   position: absolute;
   top: 40px;
   left: 40px;
+  cursor: pointer;
 `
 
 const AccountHeadMolecules = () => {
   return (
     <>
-      <LogoContainer>
-        <OurTubeLogoAtoms size={240} color={LogoColor.WHITE} />
-      </LogoContainer>
+      <Link href="/create_room">
+        <LogoContainer>
+          <OurTubeLogoAtoms size={240} color={LogoColor.WHITE} />
+        </LogoContainer>
+      </Link>
 
       <SignOutButtonMolecules />
     </>
